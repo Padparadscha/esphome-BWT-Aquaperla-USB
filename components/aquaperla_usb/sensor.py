86ac46f9d87d33582c2851bcdd19ce8a3544071e
Padparadscha <1viper37@gmx.de>
2025-01-05 05:20:20 +0100

    Update sensor.py

--- components/aquaperla_usb/sensor.py
+++ components/aquaperla_usb/sensor.py
@@ -1,31 +1,29 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor, uart
-from esphome.const import CONF_NAME, UNIT_LITERS, ICON_WATER
+from esphome.const import CONF_ID, UNIT_LITERS, ICON_WATER
 
+# Definiere Abhängigkeit von der UART-Komponente
 DEPENDENCIES = ["uart"]
 
+# Namespace für die benutzerdefinierte C++-Komponente
 aquaperla_ns = cg.esphome_ns.namespace("aquaperla_usb")
 AqaPerlaSensor = aquaperla_ns.class_("AqaPerlaSensor", cg.PollingComponent, uart.UARTDevice)
 
+# Schema für die YAML-Konfiguration
 CONFIG_SCHEMA = sensor.sensor_schema(UNIT_LITERS, ICON_WATER, 2).extend(
     {
-        cv.GenerateID(): cv.declare_id(AqaPerlaSensor),
+        cv.GenerateID(): cv.declare_id(AqaPerlaSensor),  # ID der benutzerdefinierten Komponente
     }
 )
 
+# Methode zur Code-Generierung
 async def to_code(config):
-    # Initialisiere den Sensor
+    # Initialisiere die C++-Komponente
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
-    
-    # Automatische UART-Konfiguration
-    uart_config = uart.UARTDeviceConfig(
-        tx_pin="TX",  # Pin kann hier geändert werden
-        rx_pin="RX",
-        baud_rate=19200,
-        data_bits=8,
-        stop_bits=1,
-        parity="NONE"
-    )
+    await cg.register_component(var, config)  # Registriere die Komponente als ESPHome-Element
+    await sensor.register_sensor(var, config)  # Registriere die Sensor-Funktionalität
+
+    # UART-Konfiguration für die Kommunikation
+    uart_config = await uart.new_uart_device(config)
     cg.add(var.init_uart(uart_config))
