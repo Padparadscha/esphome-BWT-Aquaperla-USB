58b5fcfea514911d43dc197d8147cae8d1544121
Padparadscha <1viper37@gmx.de>
2025-01-05 06:57:40 +0100

    Update aquaperla_usb.h

--- components/aquaperla_usb/aquaperla_usb.h
+++ components/aquaperla_usb/aquaperla_usb.h
@@ -1,16 +1,16 @@
 #pragma once  // Verhindert, dass die Datei mehrfach eingebunden wird
 
-#include "esphome/core/component.h"  // Basis-Komponentenklasse in ESPHome
-#include "esphome/components/uart/uart.h"  // UART-Funktionalität
-#include "esphome/components/sensor/sensor.h"  // Sensor-Basis für ESPHome
+#include "esphome/core/component.h"         // Basis-Komponente in ESPHome
+#include "esphome/components/uart/uart.h"   // UART-Funktionalität
+#include "esphome/components/sensor/sensor.h"  // Sensor-Basis in ESPHome
 
 namespace aquaperla_usb {
 
 /**
- * @brief Klasse für die AqaPerla-Sensor-Integration.
+ * @brief AqaPerla-Sensor-Klasse für die Integration in ESPHome.
  * 
- * Diese Klasse ermöglicht die Kommunikation mit einem AqaPerla-Gerät
- * über UART und das Abfragen von Sensorwerten.
+ * Diese Klasse implementiert die Kommunikation mit dem AqaPerla-Gerät
+ * über UART und die Abfrage spezifischer Sensorwerte.
  */
 class AqaPerlaSensor : public esphome::PollingComponent,  // Ermöglicht zyklische Updates
                        public esphome::uart::UARTDevice {  // Unterstützt UART-Kommunikation
@@ -20,14 +20,12 @@ class AqaPerlaSensor : public esphome::PollingComponent,  // Ermöglicht zyklisc
    * 
    * @param parent UART-Komponente, mit der dieses Gerät verbunden ist.
    */
-  explicit AqaPerlaSensor(esphome::uart::UARTComponent *parent)
-      : esphome::uart::UARTDevice(parent) {}
+  explicit AqaPerlaSensor(esphome::uart::UARTComponent *parent);
 
   /**
    * @brief Initialisiert den Sensor.
    * 
-   * Diese Methode wird einmal beim Start von ESPHome aufgerufen und
-   * dient der Einrichtung des Sensors.
+   * Diese Methode wird einmal beim Start von ESPHome aufgerufen.
    */
   void setup() override;
 
@@ -42,24 +40,24 @@ class AqaPerlaSensor : public esphome::PollingComponent,  // Ermöglicht zyklisc
   /**
    * @brief Sensor für den Wasserverbrauch der letzten 24 Stunden.
    */
-  esphome::sensor::Sensor verbrauch_24h;
+  esphome::sensor::Sensor *verbrauch_24h = new esphome::sensor::Sensor();
 
  private:
   /**
    * @brief Sendet ein Kommando an das AqaPerla-Gerät.
    * 
-   * Diese Funktion sendet ein Kommando gemäß dem Protokollaufbau
-   * an das angeschlossene Gerät.
+   * Diese Methode erstellt eine Nachricht basierend auf dem definierten
+   * Protokollaufbau und sendet sie über UART.
    * 
-   * @param command Das zu sendende Kommando (siehe Protokolldokumentation).
+   * @param command Das zu sendende Kommando.
    */
   void send_command(uint8_t command);
 
   /**
    * @brief Verarbeitet die Antwort des Geräts.
    * 
-   * Diese Methode liest die Antwort aus dem UART-Puffer,
-   * dekodiert die Daten und aktualisiert die zugehörigen Sensorwerte.
+   * Diese Methode liest die Antwort aus dem UART-Puffer, überprüft die Daten
+   * und aktualisiert den Sensorwert.
    */
   void process_response();
 };
