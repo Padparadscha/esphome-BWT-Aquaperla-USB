e64d7dcab64391b20543cef86fc2f094baa27c3d
Padparadscha <1viper37@gmx.de>
2025-01-05 07:12:46 +0100

    Update __init__.py

--- components/aquaperla_usb/__init__.py
+++ components/aquaperla_usb/__init__.py
@@ -1,5 +1,20 @@
-#__init__.py
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import sensor, uart
+from esphome.const import CONF_ID
 
-"""ESPHome Integration für den AqaPerla-Sensor."""
+aquaperla_usb_ns = cg.esphome_ns.namespace('aquaperla_usb')
+AqaPerlaSensor = aquaperla_usb_ns.class_('AqaPerlaSensor', cg.Component, uart.UARTDevice)
 
-# Platz für eventuelle spätere Erweiterungen oder Initialisierungen
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(AqaPerlaSensor),
+    })
+    .extend(cv.COMPONENT_SCHEMA)
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
